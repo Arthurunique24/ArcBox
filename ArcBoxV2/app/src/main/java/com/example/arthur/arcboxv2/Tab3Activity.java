51e5ab9c353e2f05400a1717a5f52205f4e8c08c
Arthurunique24 <arthurunique24@gmail.com>
2016-11-27 23:46:12 +0300

    new version with swipe tabs

---
+++ ArcBoxV2/app/src/main/java/com/example/arthur/arcboxv2/Tab3Activity.java
@@ -0,0 +1,18 @@
+package com.example.arthur.arcboxv2;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Activity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_tab3, container, false);
+        return rootView;
+    }
+}
